95871b8ca7dd070dc030ae3ef715beb52764da17
Calvin Furano <calvinfurano@gmail.com>
2019-04-26 16:02:44 -0600

    Add parameter testing.

--- test/Toadstool.UnitTests/Fixtures/StockItem.cs
+++ test/Toadstool.UnitTests/Fixtures/StockItem.cs
@@ -4,5 +4,8 @@ namespace Toadstool.UnitTests.Fixtures
     {
         public int StockItemID { get; set; }
         public string StockItemName { get; set; }
+        public string Brand { get; set; }
+        public bool IsChillerStock { get; set; }
+        public byte[] Photo { get; set; }
     }
 }
--- test/Toadstool.UnitTests/WideWorldImportersTests.cs
+++ test/Toadstool.UnitTests/WideWorldImportersTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 using Npgsql;
 using Toadstool.UnitTests.Fixtures;
@@ -13,6 +14,12 @@ namespace Toadstool.UnitTests
     {
         private static Func<IDbConnection> _postgresConnection = () => new NpgsqlConnection("Host=localhost;Database=wide_world_importers_pg;Username=postgres;Password=toadstool");
         private static Func<IDbConnection> _sqlServerConnection = () => new SqlConnection("Server=localhost;Uid=sa;Pwd=Toadstool123;Database=WideWorldImporters");
+
+        public static IEnumerable<object[]> GetSelectTestCases()
+        {
+            yield return new object[] { _postgresConnection, "SELECT stock_item_id, stock_item_name FROM warehouse.stock_items ORDER BY stock_item_id LIMIT 10" };
+            yield return new object[] { _sqlServerConnection, "SELECT TOP 10 StockItemID, StockItemName FROM Warehouse.StockItems ORDER BY StockItemID" };
+        }
 
         [Theory]
         [MemberData(nameof(GetSelectTestCases))]
@@ -31,24 +38,72 @@ namespace Toadstool.UnitTests
             Assert.NotNull(results);
             Assert.NotEmpty(results);
             Assert.Equal(10, results.Count);
-            Assert.Equal(1, results[0].StockItemID);
-            Assert.Equal("USB missile launcher (Green)", results[0].StockItemName);
-            Assert.Equal(10, results[9].StockItemID);
-            Assert.Equal("USB food flash drive - chocolate bar", results[9].StockItemName);
+            var first = results.First();
+            Assert.Equal(1, first.StockItemID);
+            Assert.Equal("USB missile launcher (Green)", first.StockItemName);
+            var last = results.Last();
+            Assert.Equal(10, last.StockItemID);
+            Assert.Equal("USB food flash drive - chocolate bar", last.StockItemName);
+            Assert.Equal("USB food flash drive - chocolate bar", last.StockItemName);
         }
 
-        public static IEnumerable<object[]> GetSelectTestCases()
+        public static IEnumerable<object[]> GetSelectWithParametersTestCases()
         {
-            yield return new object[]
-            {
-                _postgresConnection,
-                "SELECT stock_item_id, stock_item_name FROM warehouse.stock_items ORDER BY stock_item_id LIMIT 10"
-            };
-            yield return new object[] {
-                _sqlServerConnection,
-                "SELECT TOP 10 StockItemID, StockItemName FROM Warehouse.StockItems ORDER BY StockItemID"
-            };
+            yield return new object[] { _postgresConnection, "SELECT stock_item_id, stock_item_name FROM warehouse.stock_items WHERE supplier_id = @supplierId AND tax_rate = @taxRate ORDER BY stock_item_id" };
+            yield return new object[] { _sqlServerConnection, "SELECT StockItemID, StockItemName FROM Warehouse.StockItems WHERE SupplierId = @supplierId AND TaxRate = @taxRate  ORDER BY StockItemID" };
         }
 
+        [Theory]
+        [MemberData(nameof(GetSelectWithParametersTestCases))]
+        public async Task ShouldSelectFromStockItemsWithParameters(Func<IDbConnection> dbConnection, string query)
+        {
+            //Given
+            var context = new DbContext()
+                .WithConnection(dbConnection);
+
+            //When
+            var results = await context
+                .Query(query)
+                .WithParameter("supplierId", 2)
+                .WithParameter("brand", null)
+                .WithParameter("taxRate", 15.0)
+                .AsListOf<StockItem>();
+
+            //Then
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+            Assert.Equal(3, results.Count);
+            var first = results.First();
+            Assert.Equal(150, first.StockItemID);
+            Assert.Equal("Pack of 12 action figures (variety)", first.StockItemName);
+            var last = results.Last();
+            Assert.Equal(152, last.StockItemID);
+            Assert.Equal("Pack of 12 action figures (female)", last.StockItemName);
+        }
+
+        public static IEnumerable<object[]> GetSelectBooleanTestCases()
+        {
+            yield return new object[] { _postgresConnection, "SELECT stock_item_id, stock_item_name, is_chiller_stock FROM warehouse.stock_items WHERE is_chiller_stock = true" };
+            yield return new object[] { _sqlServerConnection, "SELECT StockItemID, StockItemName, IsChillerStock FROM Warehouse.StockItems WHERE IsChillerStock = 1" };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSelectBooleanTestCases))]
+        public async Task ShouldSelectBoolean(Func<IDbConnection> dbConnection, string query)
+        {
+            //Given
+            var context = new DbContext()
+                .WithConnection(dbConnection);
+
+            //When
+            var results = await context
+                .Query(query)
+                .AsListOf<StockItem>();
+
+            //Then
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+            Assert.All(results, si => { Assert.True(si.IsChillerStock); });
+        }
     }
 }
